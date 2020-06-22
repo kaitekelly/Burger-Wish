e4c2a36b2df37fdd22a25cbe3b2a1d52d5594b59
Kathleen Kelly <murphy.kaite@gmail.com>
2020-06-21 19:44:56 -0700

    burger controller js code completed

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-let routes = require("./controllers/burgerController.js");
+let routes = require("./controllers/burger_controller.js");
 
 app.use(routes);
 
