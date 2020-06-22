35604709bdc1070e34a194278a0b351b9b7ef06e
Kathleen Kelly <murphy.kaite@gmail.com>
2020-06-21 18:28:20 -0700

    added connection and orm code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INTO burgers (name) VALUES ('Bobbi');
+INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name) VALUES ('Owens');
