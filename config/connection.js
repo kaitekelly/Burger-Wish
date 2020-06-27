2915014e2843445c311e694f4836ebffb5b818b9
Kathleen Kelly <murphy.kaite@gmail.com>
2020-06-27 15:34:38 -0700

    updated config file

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: process.env.DB_password,
+        password: "whitecat1",
         database: "burgers_db"
     })
 }
